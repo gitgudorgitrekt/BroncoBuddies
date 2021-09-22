b9fb4eea83cdba6c68449cb0f802c617665d621c
William Armstrong <71415525+wxarmstrong@users.noreply.github.com>
2021-09-21 18:39:08 -0700

    Add files via upload

--- wa/api/script.js
+++ wa/api/script.js
@@ -2,6 +2,7 @@ var http = require('http');
 var uc = require('upper-case');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(uc.upperCase("Hello World!"));
+  res.write(uc.upperCase("Hello World!<br>"));
+  res.write("CS 4800 - William Armstrong");
   res.end();
 }).listen(8080); 
