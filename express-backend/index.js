5687ae363aa0dfc599e0bf5c0562bbd01e02a55a
Clarence-Hugues Domond <clarencehd@outlook.com>
2021-10-04 19:07:02 -0700

    major changes to backend layout, set up for heroku deployment

--- express-backend/index.js
+++ express-backend/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
+dotenv.config();
 
 const app = express();
 const http = require('http').createServer
