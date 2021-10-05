7c0486e2b78d9eed9be37628e1087cc9dd1352f9
Clarence-Hugues Domond <clarencehd@outlook.com>
2021-10-05 00:24:10 -0700

    bug fixes

--- express-backend/index.js
+++ express-backend/index.js
@@ -7,10 +7,9 @@ import path from 'path'
 import postRoutes from './routes/posts.js';
 
 const app = express();
-// const http = require('http').createServer
-
 dotenv.config();
 
+const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '../my-app/build')))
 
 app.get('*', (req, res) => {
@@ -30,7 +29,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
-	.then(() => app.listen(PORT, () => console.log('Server running on port: ${PORT}')))
+	.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 	.catch((error) => console.log(error.message));
-
-mongoose.set('useFindAndModify', false);
