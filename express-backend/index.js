cf4e298058f2d28f320df35107025fb7a89ff1e5
Clarence-Hugues Domond <clarencehd@outlook.com>
2021-10-05 00:52:23 -0700

    bug fix

--- express-backend/index.js
+++ express-backend/index.js
@@ -30,6 +30,7 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+const url = process.env.CONNECTION_URL
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
 	.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 	.catch((error) => console.log(error.message));
