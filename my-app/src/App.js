6405829ec1961f9871b7462878389d5063bc7893
Ben Aldrich <benaldrich2000@gmail.com>
2021-10-08 13:25:01 -0700

    UI work for frontend
    started using tutorials to get project working
    bronco buddies now acting as a banner in our project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,20 @@
 import Button from 'react-bootstrap/Button';
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 
-function App() {
+//import name from './images/name.png';
+
+const App = () => {
     return (
-        // From Clarence
-        <>
-          <Button>
-              HTTP API using 3rd party library React Bootstrap
-          </Button>
-        </>
+        <Container maxidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography
+                    variant="h2"
+                    align="center">
+                        Bronco Buddies
+                </Typography>
+            </AppBar>
+        </Container>
     );
 }
 
