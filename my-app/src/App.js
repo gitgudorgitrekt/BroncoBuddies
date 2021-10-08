b196c9e65c75a9dbf28849bee185f4721424863c
Ben Aldrich <benaldrich2000@gmail.com>
2021-10-08 15:49:09 -0700

    Started Adding Styling
    Basic Styling from tutorial 
    update to something closer to design later

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,23 @@
-import Button from 'react-bootstrap/Button';
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
+import useStyles from './styles'
 //import name from './images/name.png';
 
 const App = () => {
+    const classes = useStyles();
     return (
         <Container maxidth="lg">
-            <AppBar position="static" color="inherit">
+            <AppBar className={classes.appBar} position="static" color="inherit">
                 <Typography
-                    variant="h2"
-                    align="center">
+                    className={classes.heading} variant="h2" align="center">
                         Bronco Buddies
                 </Typography>
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify="space between"
-                    alignItems="strech" spacing={3}>
+                    <Grid container justify="space between" alignItems="strech" spacing={3}>
                         <Grid item xs={12} sm={7}>
                             <Posts />
                             </Grid>
