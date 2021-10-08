f9798a20c3967181593777007f531651b7d2c6e3
Ben Aldrich <benaldrich2000@gmail.com>
2021-10-08 14:19:05 -0700

    Added UI Components
    Added Post and Form components to use later for site

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,8 @@
 import Button from 'react-bootstrap/Button';
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
-
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 //import name from './images/name.png';
 
 const App = () => {
@@ -14,6 +15,19 @@ const App = () => {
                         Bronco Buddies
                 </Typography>
             </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space between"
+                    alignItems="strech" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                            </Grid>
+                        <Grid item xs={12} sm={7}>
+                            <Form />
+                            </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
         </Container>
     );
 }
