4da0b0019994606e4d5edbb11211953fba1dba80
Amal Anu <68039614+amalanu@users.noreply.github.com>
2021-10-12 21:42:29 -0700

    Updated registration controller

--- express-backend/controllers/registration.js
+++ express-backend/controllers/registration.js
@@ -4,9 +4,30 @@ import bcrypt from 'bcrypt';
 export const register = async (req, res) => {
 	console.log(req.body);
 
-	const { username, password } = req.body;
+	const { username, password: plainTextPassword } = req.body;
 
-	console.log(await bcrypt.hash(password, 15));
+	if (!username || typeof username != 'string') {
+		return res.json({ status: 'error', error: 'Invalid username'})
+	}
+
+	if (!plainTextPassword || typeof plainTextPassword != 'string') {
+		return res.json({ status: 'error', error: 'Invalid password'})
+	}
+
+	console.log(await bcrypt.hash(plainTextPassword, 15));
+
+	try{
+		const response await User.create({
+			username,
+			password
+		})
+		console.log('User created:', response);
+	} catch(error) {
+		if (error.code == 11000) {
+			return res.json({ status: 'error', error: 'Username already in use'})
+		}
+		throw error;
+	}
 
 	res.json({ status: 'ok' })
 }
