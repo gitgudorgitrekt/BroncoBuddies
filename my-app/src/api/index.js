b4baf5b15f2f6d7ed8623b45d336a0a2bd4597b4
Ben Aldrich <benaldrich2000@gmail.com>
2021-10-19 16:28:18 -0700

    frontend added

--- my-app/src/api/index.js
+++ my-app/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 //url pointing to backend route TODO work with backend team
-const url = 'http://localhost:5000/posts';
+const url = 'https://broncobuddies.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
