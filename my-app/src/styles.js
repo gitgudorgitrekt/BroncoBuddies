6abc660e918e13a5a6d78ceee4b92f3a46844407
celinem13 <36501296+celinem13@users.noreply.github.com>
2021-10-19 18:34:39 -0700

    navigation bar

--- my-app/src/styles.js
+++ my-app/src/styles.js
@@ -15,4 +15,9 @@ export default makeStyles(() =>({
       image: {
         marginLeft: '15px',
       },
+
+      topnav: {
+        backgroundcolor: rgba(0,183,255, 1),
+        overflow: hidden,
+      },
 }));
