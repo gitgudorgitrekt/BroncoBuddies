59b521cbcb384f166fa3e3b8d40e104322c297ce
Ben Aldrich <benaldrich2000@gmail.com>
2021-10-19 19:51:34 -0700

    Forms and Posts work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import React, {useEffect} from 'react';
-import { Container, AppBar, Typography, Grow, Grid, ExpansionPanelActions} from '@material-ui/core';
+import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import {useDispatch} from 'react-redux';
 
 import {getPosts} from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
-import useStyles from './styles'
+import useStyles from './styles';
 //import name from './images/name.png';
 
 // import Enzyme, { shallow } from 'enzyme';
--- my-app/src/components/Form/Form.js
+++ my-app/src/components/Form/Form.js
@@ -1,21 +1,31 @@
 import React, {useState} from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core'; 
 import FileBase from 'react-file-base64';
+
+import { useDispatch } from 'react-redux';
+
 import useStyles from './styles';
+import { createPost } from '../../actions/posts';
 
 const Form =  () => {
     const [postData, setPostData] = useState({
         creator: '', title: '', message: '',tags: '', selectedFile:'' 
     });
     const classes = useStyles();
+    const dispatch = useDispatch();
+    const handleSubmit = (e) => {
+        e.preventDeafualt();
 
-    const handleSubmit = () => {
+        dispatch(createPost(postData));
+    }
+
+    const clear = () => {
 
     }
 
     return(
         <Paper className = {classes.paper}>
-            <form autoComplete="off" noValidate className={classes.form}
+            <form autoComplete="off" noValidate className={'${classes.root} ${classes.form}'}
             onSubmit={handleSubmit}>
                 <Typography variant="h6">Hang Outs</Typography>
                 <TextField 
@@ -30,21 +40,21 @@ const Form =  () => {
                     variant="outlined" 
                     label="Title"
                     fullWidth
-                    value={postData.creator}
+                    value={postData.title}
                     onChange={(e) => setPostData({...postData, title: e.target.value})}/>
                 <TextField 
                     name="message" 
                     variant="outlined" 
                     label="Message"
                     fullWidth
-                    value={postData.creator}
+                    value={postData.message}
                     onChange={(e) => setPostData({...postData, message: e.target.value})}/>
                 <TextField 
                     name="tags" 
                     variant="outlined" 
                     label="Tags"
                     fullWidth
-                    value={postData.creator}
+                    value={postData.tags}
                     onChange={(e) => setPostData({...postData, tags: e.target.value})}/>
                 <dive className={classes.fileInput}>
                     <FileBase
@@ -52,10 +62,14 @@ const Form =  () => {
                         multiple={false}
                         onDone={({base64})=>setPostData({...postData, selectedFile: base64})}
                     />
+                     </dive>
                 <Button className={classes.buttonSubmit} variant="container" color="primary" size="large" type="submit" fullWidth>
                     Submit
                 </Button>
-                </dive>
+                <Button variant="contained" color="secodary" size="small" onClick={clear} fullWidth>
+                    Clear
+                </Button>
+               
             </form>
         </Paper>
     );
