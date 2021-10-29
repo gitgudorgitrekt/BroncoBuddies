b6050a8ccbab058f060ddf7ba4a5d03ce24f2574
Ben <benaldrich2000@gmail.com>
2021-10-29 16:19:25 -0700

    bug fix with sending posts

--- my-app/src/api/index.js
+++ my-app/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 //url pointing to backend route TODO work with backend team
-const url = 'https://broncobuddies.herokuapp.com/posts';
+//const url = 'https://broncobuddies.herokuapp.com/posts';
+const url = 'http:localhost:5000/posts';
 
 export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost)
