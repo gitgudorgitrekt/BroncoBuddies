c0d38aa5159011169c37e30fb15696ec7c41761d
Clarence <clarencehd@outlook.com>
2021-11-08 21:20:24 -0800

    added update functionality

--- my-app/src/components/Posts/Posts.js
+++ my-app/src/components/Posts/Posts.js
@@ -5,7 +5,7 @@ import { useSelector} from 'react-redux';
 import Post from './Post/Post';
 import useStyles from './styles';
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
     const posts = useSelector((state) => state.posts);
     const classes = useStyles();
 
@@ -13,13 +13,13 @@ const Posts = () => {
     console.log(isArr);
     return(
         !posts.length ? <CircularProgress/> : (
-          <Grid className={classes.container} container alignItems="stretch" spacing = {3}> 
+          <Grid className={classes.container} container alignItems="stretch" spacing={3}>
               {posts.map((post) =>(
                       <Grid key={post.id} item xs={12} sm={6}>
-                            <Post post={post}/>
+                            <Post post={post} setCurrentId={setCurrentId}/>
                       </Grid>
               ))}
-            </Grid>  
+            </Grid>
         )
     );
 }
