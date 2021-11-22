1a4304144648f2ac2c5c4aafd1ad8b151742997e
Ben <benaldrich2000@gmail.com>
2021-11-21 18:00:40 -0800

    small comment added

--- express-backend/index.js
+++ express-backend/index.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(express.json({ limit: "30mb", extended: true}));
 app.use(express.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+//TODO change * later to allow specific clients to access our server
 app.get('/cors', (req, res) => {
 	res.set('Access-Control-Allow-Origin', '*');
 	res.send({ "msg": "This has CORS enabled 🎈" })
