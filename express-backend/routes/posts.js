860d4e807b11dae4657ee4b4d738d2bc5e69cf7d
Ben <benaldrich2000@gmail.com>
2021-11-23 15:14:34 -0800

    Revert "cors "
    This reverts commit b36fb51141c1d6f70106d053e3f84435ffb724c2.

--- express-backend/routes/posts.js
+++ express-backend/routes/posts.js
@@ -1,14 +1,9 @@
 import express from 'express';
-import cors from 'cors';
+
 import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js'
 
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
- }
 const router = express.Router();
-router.use(cors(corsOptions));
+
 router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
