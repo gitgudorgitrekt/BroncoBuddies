477aa43191fe57bd32f677348572abcaa2940764
celinem13 <36501296+celinem13@users.noreply.github.com>
2021-11-27 15:46:35 -0800

    updating bar
    changing items in bar.js test

--- my-app/src/components/Bar/Bar.js
+++ my-app/src/components/Bar/Bar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AppBar, Button, Toolbar, Typography, Avatar } from '@material-ui/core'
+import { AppBar, Button, Toolbar, Typography, Menu, MenuItem, Avatar } from '@material-ui/core'
 import { Link } from 'react-router-dom';
 
 import useStyles from './barstyles'
@@ -11,21 +11,11 @@ const Bar = () => {
 
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
-            <div className={classes.brandContainer}>
-                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Bronco Buddies</Typography>
-                <img className={classes.image} src={buddies} alt="icon" height="50" />
-            </div>
-            <Toolbar className={classes.toolbar}>
-                {user ?  (
-                    <div className={classes.profile}>
-                        <Avatar className={classes.yellow} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
-                        <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
-                        <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
-                    </div>
-                ) : (
-                    <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
-                )}
-            </Toolbar>
+            <Menu id="menu-appbar">
+                <MenuItem onClick={handleCloseNavMenu} component={Link} to="/test">
+                    <Typography textAlign="center">Test</Typography>
+                </MenuItem>
+            </Menu>
         </AppBar>
     );
 };
