7608865576ead4ff94613830c5236a60242e0007
celinem13 <36501296+celinem13@users.noreply.github.com>
2021-11-28 01:24:32 -0800

    Update Tags.js

--- my-app/src/components/Tags/Tags.js
+++ my-app/src/components/Tags/Tags.js
@@ -19,24 +19,41 @@ const Auth = () => {
                 <Box sx={{ background: 'white' }} className={classes.LoginBox}>
                     <FormControl component="fieldset">
                         <FormLabel component="legend">Gender</FormLabel>
-                            <RadioGroup aria-label="gender" defaultValue="female" name="radio-buttons-group">
+                            <RadioGroup aria-label="gender" name="radio-buttons-group">
                                 <FormControlLabel value="female" control={<Radio />} label="Female" />
                                 <FormControlLabel value="male" control={<Radio />} label="Male" />
                                 <FormControlLabel value="other" control={<Radio />} label="Other" />
                             </RadioGroup>
                     </FormControl>
-                    <FormControl fullWidth sx={{ m: 1 }}>
-                            <Button variant="contained">Complete Profile</Button>
+                    <FormControl component="fieldset">
+                        <FormLabel component="legend">Are You A Transfer?</FormLabel>
+                            <RadioGroup aria-label="transfer" name="radio-buttons-group">
+                                <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                                <FormControlLabel value="no" control={<Radio />} label="no" />
+                            </RadioGroup>
+                    </FormControl>
+                    <FormControl component="fieldset">
+                        <FormLabel component="legend">Year</FormLabel>
+                            <RadioGroup aria-label="Year" name="radio-buttons-group">
+                                <FormControlLabel value="freshman" control={<Radio />} label="Freshman" />
+                                <FormControlLabel value="sophomore" control={<Radio />} label="Sophomore" />
+                                <FormControlLabel value="junior" control={<Radio />} label="Junior" />
+                                <FormControlLabel value="senior" control={<Radio />} label="Senior" />
+                            </RadioGroup>
                     </FormControl>
                     <FormControl>
                         <FormGroup>
-                            <FormControlLabel control={<Checkbox />} label="Partying" />
-                            <FormControlLabel control={<Checkbox />} label="Studying" />
-                            <FormControlLabel control={<Checkbox />} label="Gaming" />
-                            <FormControlLabel control={<Checkbox />} label="Movies and Shows" />
-                            <FormControlLabel control={<Checkbox />} label="Relationship" />
-                            <FormControlLabel control={<Checkbox />} label="Food" />
+                            <FormLabel component="legend">Interest tags</FormLabel>
+                                <FormControlLabel control={<Checkbox />} label="Partying" />
+                                <FormControlLabel control={<Checkbox />} label="Studying" />
+                                <FormControlLabel control={<Checkbox />} label="Gaming" />
+                                <FormControlLabel control={<Checkbox />} label="Movies and Shows" />
+                                <FormControlLabel control={<Checkbox />} label="Relationship" />
+                                <FormControlLabel control={<Checkbox />} label="Food" />
                         </FormGroup>
+                    </FormControl>
+                    <FormControl fullWidth sx={{ m: 1 }}>
+                            <Button variant="contained">Complete Profile</Button>
                     </FormControl>
                 </Box>
             </div>
