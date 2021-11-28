f5cb8908b28843955b6fd8aea795c65a3d7e4f6c
celinem13 <36501296+celinem13@users.noreply.github.com>
2021-11-27 22:45:41 -0800

    box wrap

--- my-app/src/components/Register/Register.js
+++ my-app/src/components/Register/Register.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 import FormControl from '@mui/material/FormControl'
 const Auth = () => {
     const state = null;
@@ -8,18 +9,20 @@ const Auth = () => {
         <div>
             <h1>Create An Account Here</h1>
             <div>
-                <FormControl fullWidth sx={{ m: 1 }}>
-                    <TextField id="standard-basic" label="First name" variant="standard" />
+                <Box>
+                    <FormControl fullWidth sx={{ m: 1 }}>
+                        <TextField id="standard-basic" label="First name" variant="standard" />
+                    </FormControl>
+                    <FormControl fullWidth sx={{ m: 1 }}>
+                        <TextField id="standard-basic" label="Last name" variant="standard" />
+                    </FormControl>
+                    <FormControl fullWidth sx={{ m: 1 }}>
+                        <TextField id="standard-basic" label="Email" variant="standard" />
+                    </FormControl>
+                    <FormControl fullWidth sx={{ m: 1 }}>
+                        <TextField id="standard-basic" label="Password" variant="standard" />
                 </FormControl>
-                <FormControl fullWidth sx={{ m: 1 }}>
-                    <TextField id="standard-basic" label="Last name" variant="standard" />
-                </FormControl>
-                <FormControl fullWidth sx={{ m: 1 }}>
-                    <TextField id="standard-basic" label="Email" variant="standard" />
-                </FormControl>
-                <FormControl fullWidth sx={{ m: 1 }}>
-                    <TextField id="standard-basic" label="Password" variant="standard" />
-                </FormControl>
+                </Box>
             </div>
         </div>
     )
