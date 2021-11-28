b768a0eef82fe20b9ea340da3403694a393b15ee
celinem13 <36501296+celinem13@users.noreply.github.com>
2021-11-27 22:32:00 -0800

    add elements to register.js

--- my-app/src/components/Register/Register.js
+++ my-app/src/components/Register/Register.js
@@ -5,7 +5,12 @@ const Auth = () => {
 
     return (
         <div>
-            AUTH
+            <h1>Create An Account Here</h1>
+
+            <TextField id="standard-basic" label="First name" variant="standard" />
+            <TextField id="standard-basic" label="Last name" variant="standard" />
+            <TextField id="standard-basic" label="Email" variant="standard" />
+            <TextField id="standard-basic" label="Password" variant="standard" />
         </div>
     )
 }
