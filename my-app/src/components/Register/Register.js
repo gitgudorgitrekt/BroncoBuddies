4906630be72063cd905a5f79b5f1b47eb51e73eb
celinem13 <36501296+celinem13@users.noreply.github.com>
2021-11-27 23:06:50 -0800

    welcome to the club

--- my-app/src/components/Register/Register.js
+++ my-app/src/components/Register/Register.js
@@ -1,28 +1,39 @@
 import React from 'react'
+import Spongebob from '../../images/spongebob-welcome.gif'
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import FormControl from '@mui/material/FormControl'
+import Button from '@mui/material/Button'
+import useStyles from './styles'
 const Auth = () => {
     const state = null;
-
+    const classes = useStyles();
     return (
         <div>
             <h1>Create An Account Here</h1>
             <div>
-                <Box>
-                    <FormControl fullWidth sx={{ m: 1 }}>
-                        <TextField id="standard-basic" label="First name" variant="standard" />
-                    </FormControl>
-                    <FormControl fullWidth sx={{ m: 1 }}>
-                        <TextField id="standard-basic" label="Last name" variant="standard" />
-                    </FormControl>
-                    <FormControl fullWidth sx={{ m: 1 }}>
-                        <TextField id="standard-basic" label="Email" variant="standard" />
-                    </FormControl>
-                    <FormControl fullWidth sx={{ m: 1 }}>
-                        <TextField id="standard-basic" label="Password" variant="standard" />
-                </FormControl>
+                <Box sx={{ background: 'white' }} className={classes.LoginBox}>
+                        <FormControl fullWidth sx={{ m: 1 }}>
+                            <TextField id="standard-basic" label="First name" variant="standard" />
+                        </FormControl>
+                        <FormControl fullWidth sx={{ m: 1 }}>
+                            <TextField id="standard-basic" label="Last name" variant="standard" />
+                        </FormControl>
+                        <FormControl fullWidth sx={{ m: 1 }}>
+                            <TextField id="standard-basic" label="Email" variant="standard" />
+                        </FormControl>
+                        <FormControl fullWidth sx={{ m: 1 }}>
+                            <TextField id="standard-basic" label="Password" variant="standard" />
+                        </FormControl>
+                        <FormControl fullWidth sx={{ m: 1 }}>
+                            <Button variant="contained" color="success">Register Account</Button>
+                        </FormControl>
                 </Box>
+                <center>
+                    <div>
+                        <img src={Spongebob} alt="Welcome to the Club Squidward" height="800" />
+                    </div>
+                </center>
             </div>
         </div>
     )
---
+++ my-app/src/components/Register/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  LoginBox: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '10px 50px',
+  }
+}));
