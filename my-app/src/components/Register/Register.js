c82722438dcb312fefeb711dd781a1cf8cd2cff6
celinem13 <36501296+celinem13@users.noreply.github.com>
2021-11-27 22:44:35 -0800

    Adding form control

--- my-app/src/components/Register/Register.js
+++ my-app/src/components/Register/Register.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import TextField from '@mui/material/TextField';
-import Box from '@material-ui/core'
+import FormControl from '@mui/material/FormControl'
 const Auth = () => {
     const state = null;
 
@@ -8,12 +8,18 @@ const Auth = () => {
         <div>
             <h1>Create An Account Here</h1>
             <div>
-                <Box>
+                <FormControl fullWidth sx={{ m: 1 }}>
                     <TextField id="standard-basic" label="First name" variant="standard" />
+                </FormControl>
+                <FormControl fullWidth sx={{ m: 1 }}>
                     <TextField id="standard-basic" label="Last name" variant="standard" />
+                </FormControl>
+                <FormControl fullWidth sx={{ m: 1 }}>
                     <TextField id="standard-basic" label="Email" variant="standard" />
+                </FormControl>
+                <FormControl fullWidth sx={{ m: 1 }}>
                     <TextField id="standard-basic" label="Password" variant="standard" />
-                </Box>
+                </FormControl>
             </div>
         </div>
     )
