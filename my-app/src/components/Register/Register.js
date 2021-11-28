17e3424ae974616cd84b4ab435cb13ed2dc6276c
celinem13 <36501296+celinem13@users.noreply.github.com>
2021-11-27 22:42:33 -0800

    Updating register.js

--- my-app/src/components/Register/Register.js
+++ my-app/src/components/Register/Register.js
@@ -1,17 +1,20 @@
 import React from 'react'
-import TextField from '@material-ui/core'
-
+import TextField from '@mui/material/TextField';
+import Box from '@material-ui/core'
 const Auth = () => {
     const state = null;
 
     return (
         <div>
             <h1>Create An Account Here</h1>
-
-            <TextField id="standard-basic" label="First name" variant="standard" />
-            <TextField id="standard-basic" label="Last name" variant="standard" />
-            <TextField id="standard-basic" label="Email" variant="standard" />
-            <TextField id="standard-basic" label="Password" variant="standard" />
+            <div>
+                <Box>
+                    <TextField id="standard-basic" label="First name" variant="standard" />
+                    <TextField id="standard-basic" label="Last name" variant="standard" />
+                    <TextField id="standard-basic" label="Email" variant="standard" />
+                    <TextField id="standard-basic" label="Password" variant="standard" />
+                </Box>
+            </div>
         </div>
     )
 }
