7d01a07fca84fd2f81ec12c7fb2ac64c5465301a
Ben <benaldrich2000@gmail.com>
2021-11-27 23:46:01 -0800

    Getting rid of mistakes

--- express-backend/routes/posts.js
+++ express-backend/routes/posts.js
@@ -3,13 +3,6 @@ import express from 'express';
 import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js'
 
 const router = express.Router();
-router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-  });
 router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
