8e1d457df392f032fe2bce0a24992f246e7f0565
celinem13 <36501296+celinem13@users.noreply.github.com>
2021-11-29 14:13:38 -0800

    Update BuddyFilter.js

--- my-app/src/components/BuddyFilter/BuddyFilter.js
+++ my-app/src/components/BuddyFilter/BuddyFilter.js
@@ -1,12 +1,25 @@
 import React from 'react'
-
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 const BuddyFilter = () => {
     const state = null;
-
+    const Item = styled(Paper)(({ theme }) => ({
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+      }));
     return (
-        <div>
-            AUTH
-        </div>
+        <Grid container spacing={2}>
+            <Grid item xs={3}>
+                <Item>Filter</Item>
+            </Grid>
+            <Grid item xs={9}>
+                <Item>Results</Item>
+            </Grid>
+        </Grid>
     )
 }
 
