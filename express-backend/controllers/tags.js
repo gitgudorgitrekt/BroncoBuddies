6c3251f47450b747e28c2761f41cb99892797572
celinem13 <36501296+celinem13@users.noreply.github.com>
2021-11-28 16:43:27 -0800

    Update tags.js

--- express-backend/controllers/tags.js
+++ express-backend/controllers/tags.js
@@ -2,13 +2,15 @@ import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 import User from '../models/user.js';
 
-export const login = async (req, res) => {
-  let { username, password } = req.body;
-
-  let user = await User.findOne({username});
-  let result = await bcrypt.compare(password, user.password);
-
-  res.cookie('session', user._id, { signed: true });
-
+export const tags = async (req, res) => {
+  let { gender, transfer, year, tags } = req.body;
+  let userID = req.signedCookies.session;
+  let user = await User.findOne({_id:userID});
+  user.gender = gender;
+  user.transfer = transfer;
+  user.year = year;
+  user.tags = tags;
+  await user.save();
   return res.json({ success: result });
 }
+ 
