1d89bdc1c7883bf4ce0d7f7e118b996c40675dff
Ben <benaldrich2000@gmail.com>
2021-11-30 10:29:26 -0800

    small backend fix

--- express-backend/index.js
+++ express-backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json({ limit: "30mb", extended: true}));
 app.use(express.urlencoded({ limit: "30mb", extended: true}));
-app.use(cors({origin:'https://broncobuddiescpp.netlify.app'}));
+app.use(cors({origin:'https://broncobuddiescpp.netlify.app', credentials: true}));
 app.use(cookieParser('pazzw0rd$'));
 app.use('/auth', auth);
 
