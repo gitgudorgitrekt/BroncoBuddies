1c5c22be95d3866e800e3801315fa08d89166a90
Ben <benaldrich2000@gmail.com>
2021-12-02 05:26:28 -0800

    new frontend build

---
+++ my-app/build/precache-manifest.a1582bb12195dec77bd3b5e06569fa22.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb99ac58110aa7c5f92fec4104a72ecf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99d7bf250671e47f6562",
+    "url": "/static/css/main.54076823.chunk.css"
+  },
+  {
+    "revision": "ebeb7f95f9fbea1e4daa",
+    "url": "/static/js/2.82899828.chunk.js"
+  },
+  {
+    "revision": "67a39ee65e9bc69add2460a33475b901",
+    "url": "/static/js/2.82899828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d7bf250671e47f6562",
+    "url": "/static/js/main.eeb6be86.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "a13acbb9612c18d46d2ff0a29cb8560c",
+    "url": "/static/media/dutchman.a13acbb9.gif"
+  },
+  {
+    "revision": "d22b2de69ab6ab544db85d1f271c0190",
+    "url": "/static/media/group-android-chrome-512x512.d22b2de6.png"
+  },
+  {
+    "revision": "121226afe3d5b5cba6ec63b760dbb6e0",
+    "url": "/static/media/spongebob-welcome.121226af.gif"
+  }
+]);
