29f79ea6bb9bbba6c20cea07ef3d75004b591eb2
Ben <benaldrich2000@gmail.com>
2021-12-02 02:00:59 -0800

    connection updates

--- my-app/src/api/index.js
+++ my-app/src/api/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-//const url = 'http://localhost:5000/posts';
-const url = 'https://broncobuddies.herokuapp.com/posts';
+const url = 'http://localhost:5000/posts' || 'https://broncobuddies.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
